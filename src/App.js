e09b409535a2a3d59bf8cfa67233c409a1069b33
Stefan Wille <github@stefanwille.com>
2019-11-14 21:09:53 +0100

    Fix tabs

--- src/App.js
+++ src/App.js
@@ -9,128 +9,135 @@ import { pipe, subscribe } from 'wonka';
 const getToken = () => 'abcdefg12345';
 
 const client = createClient({
-	url: 'http://localhost:4000/graphql',
+  url: 'http://localhost:4000/graphql',
 
-	fetchOptions: () => {
-		const token = getToken();
-		return {
-			headers: { authorization: token ? `Bearer ${token}` : '' },
-		};
-	},
+  fetchOptions: () => {
+    const token = getToken();
+    return {
+      headers: { authorization: token ? `Bearer ${token}` : '' },
+    };
+  },
 });
 
 const getMe = gql`
-	query GetMe {
-		me {
-			firstName
-			lastName
-		}
-	}
+  query GetMe {
+    me {
+      firstName
+      lastName
+    }
+  }
 `;
 
 const DeclarativeQueryForMe = () => (
-	<Query query={getMe} variables={{}}>
-		{({ fetching, data, error, extensions }) => {
-			if (fetching) {
-				return 'Loading...';
-			} else if (error) {
-				return 'Oh no!';
-			}
-			return (
-				<p>
-					Declarative query via &lt;Query&gt;: {data.me.firstName} {data.me.lastName}
-				</p>
-			);
-		}}
-	</Query>
+  <Query query={getMe} variables={{}}>
+    {({ fetching, data, error, extensions }) => {
+      if (fetching) {
+        return 'Loading...';
+      } else if (error) {
+        return 'Oh no!';
+      }
+      return (
+        <p>
+          Declarative query via &lt;Query&gt;: {data.me.firstName}{' '}
+          {data.me.lastName}
+        </p>
+      );
+    }}
+  </Query>
 );
 
 const HookQueryForMe = () => {
-	const [ result ] = useQuery({ query: getMe });
-	const { fetching, error, data } = result;
+  const [result] = useQuery({ query: getMe });
+  const { fetching, error, data } = result;
 
-	if (fetching) {
-		return 'Fetching...';
-	}
-	if (error) {
-		return 'Error...';
-	}
+  if (fetching) {
+    return 'Fetching...';
+  }
+  if (error) {
+    return 'Error...';
+  }
 
-	return (
-		<p>
-			Declarative query via useQuery(): {data.me.firstName} {data.me.lastName}
-		</p>
-	);
+  return (
+    <p>
+      Declarative query via useQuery(): {data.me.firstName} {data.me.lastName}
+    </p>
+  );
 };
 
-const performQuery = (query) => {
-	return new Promise((resolve, reject) => {
-		const request = createRequest(query);
-		pipe(
-			client.executeQuery(request),
-			subscribe(({ data, error }) => {
-				if (error) {
-					reject(error);
-				}
-				resolve(data);
-			})
-		);
-	});
+const performQuery = query => {
+  return new Promise((resolve, reject) => {
+    const request = createRequest(query);
+    pipe(
+      client.executeQuery(request),
+      subscribe(({ data, error }) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(data);
+      })
+    );
+  });
 };
 
 const ImperativeQueryForMe = () => {
-	const [ fetching, setFetching ] = useState(false);
-	const [ error, setError ] = useState(null);
-	const [ data, setData ] = useState(null);
+  const [fetching, setFetching] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
 
-	useEffect(() => {
-		const run = async () => {
-			setFetching(true);
-			try {
-				const data = await performQuery(getMe);
-				setData(data);
-			} catch (error) {
-				setError(error);
-			} finally {
-				setFetching(false);
-			}
-		};
-		run();
-	}, []);
+  useEffect(() => {
+    const run = async () => {
+      setFetching(true);
+      try {
+        const data = await performQuery(getMe);
+        setData(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setFetching(false);
+      }
+    };
+    run();
+  }, []);
 
-	if (fetching) {
-		return 'Fetching...';
-	}
+  if (fetching) {
+    return 'Fetching...';
+  }
 
-	if (error) {
-		return 'Error...';
-	}
+  if (error) {
+    return 'Error...';
+  }
 
-	return (
-		<p>
-			Imperative query via client.executeQuery(): {data.me.firstName} {data.me.lastName}
-		</p>
-	);
+  return (
+    <p>
+      Imperative query via client.executeQuery(): {data.me.firstName}{' '}
+      {data.me.lastName}
+    </p>
+  );
 };
 
 function App() {
-	return (
-		<Provider value={client}>
-			<div className="App">
-				<header className="App-header">
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-						Learn React
-					</a>
-					<DeclarativeQueryForMe />
-					<HookQueryForMe />
-					<ImperativeQueryForMe />
-				</header>
-			</div>
-		</Provider>
-	);
+  return (
+    <Provider value={client}>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <DeclarativeQueryForMe />
+          <HookQueryForMe />
+          <ImperativeQueryForMe />
+        </header>
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
